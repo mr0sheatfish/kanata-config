476457373fecc7f4f6d93857766a263cdb7438df
mr0sheatfish <misha1zyablov@gmail.com>
2024-11-01 22:35:08 +0500

    Add space+q as esc and ctrl+y as redo

--- kanata-autostart.ps1
+++ kanata-autostart.ps1
@@ -1,7 +1,7 @@
 $StartupPath="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $ProgramName="Kanata"
-$KanataPath=(get-command windows_exec\kanata_winIOv2.exe).Path
+$KanataPath=(get-command kanata_winIOv2.exe).Path
 $KanataConfigPath="$PSScriptRoot\kanata.kbd"
-$StartupCommand="""C:\Windows\system32\conhost.exe"" --headless ""$KanataPath"" --cfg ""$KanataConfigPath"""
+$StartupCommand="""conhost.exe"" --headless ""$KanataPath"" --cfg ""$KanataConfigPath"""
 
 Set-ItemProperty -LiteralPath "$StartupPath" -Name "$ProgramName" -Value "$StartupCommand"
