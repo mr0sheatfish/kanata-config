781894c1cf24aa47fdb1b15842e43313f87ce0df
mr0sheatfish <misha1zyablov@gmail.com>
2024-11-01 20:45:26 +0500

    Sorting files by type.

--- kanata-autostart.ps1
+++ kanata-autostart.ps1
@@ -1,7 +1,7 @@
 $StartupPath="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $ProgramName="Kanata"
 $KanataPath=(get-command kanata_winIOv2.exe).Path
-$KanataConfigPath="$PSScriptRoot\kanata.kbd"
+$KanataConfigPath="$PSScriptRoot\..\kanata.kbd"
 $StartupCommand="""C:\Windows\system32\conhost.exe"" --headless ""$KanataPath"" --cfg ""$KanataConfigPath"""
 
 Set-ItemProperty -LiteralPath "$StartupPath" -Name "$ProgramName" -Value "$StartupCommand"
